f4becab67ba3faa0d56b9c8bad6f48bdf899eeaa
zidaneNS <zidane.nur004@gmail.com>
2025-12-16 21:06:27 +0700

    final touch

--- src/sections/ServiceSection.tsx
+++ src/sections/ServiceSection.tsx
@@ -85,29 +85,29 @@ export default function ServiceSection() {
       <p className="text-xl md:text-lg font-semibold text-slate-500 text-center md:max-w-1/2">Dari momen intim hingga acara besar, kami menyediakan layanan fotografi dan videografi profesional yang disesuaikan dengan kebutuhan Anda.</p>
       <div className="flex flex-col md:flex-row gap-y-8 md:gap-x-12 w-full">
         {services.map((service, idx) => (
-          <div key={idx} className="flex flex-col gap-y-4 md:gap-y-8 flex-1 rounded-md shadow-md cursor-pointer px-6 py-4 hover:scale-105 duration-300 pb-auto h-auto">
-            <service.Icon className="size-12 text-yellow-400" />
-            <h3 className="text-2xl font-bold font-playfair">{service.title}</h3>
-            <p className="text-lg text-slate-500">{service.desc}</p>
-            <p className="text-lg font-bold">What's Included</p>
-            <div className="flex flex-col gap-y-4">
+          <div key={idx} className="flex flex-col gap-y-4 md:gap-y-8 flex-1 rounded-md shadow-md cursor-pointer px-6 py-4 hover:scale-105 duration-300 pb-auto h-auto group">
+            <service.Icon className="size-8 md:size-12 text-yellow-400" />
+            <h3 className="text-xl md:text-2xl font-bold font-playfair">{service.title}</h3>
+            <p className="text-base md:text-lg text-slate-500">{service.desc}</p>
+            <p className="text-base md:text-lg font-bold">What's Included</p>
+            <div className="flex flex-col gap-y-2 md:gap-y-4">
               {service.content.map((content, id) => (
                 <div key={id} className="flex gap-x-3 items-center">
                   <span className="w-2 h-2 rounded-full bg-yellow-400" />
-                  <p className="text-lg text-slate-500">{content}</p>
+                  <p className="text-base md:text-lg text-slate-500">{content}</p>
                 </div>
               ))}
             </div>
             <span className="w-full border border-slate-300"></span>
-            <p className="text-xl font-bold text-yellow-400">Starting from IDR {convertCurrency(service.price)}</p>
-            <button className="text-center cursor-pointer py-2 px-4 rounded-md duration-300 border border-slate-200 shadow-md hover:bg-yellow-400">Get Quote</button>
+            <p className="text-lg md:text-xl font-bold text-yellow-400">Starting from IDR {convertCurrency(service.price)}</p>
+            <button onClick={() => window.location.replace('#contact')} className="text-center cursor-pointer py-2 px-4 rounded-md duration-300 border border-slate-200 shadow-md hover:bg-yellow-400 group-hover:bg-yellow-400">Get Quote</button>
           </div>
         ))}
       </div>
       <div className="flex flex-col items-center gap-y-4 md:gap-y-8 md:max-w-1/2 pt-8">
         <p className="text-xl md:text-2xl font-bold">Need Something Different?</p>
         <p className="text-center text-base md:text-lg text-slate-500">Kami juga menyediakan layanan kustom untuk kebutuhan khusus Anda. Konsultasikan project Anda dengan tim kami untuk solusi terbaik.</p>
-        <div className="flex flex-col md:flex-row items-center gap-y-8 md:gap-x-16">
+        <div className="flex flex-col md:flex-row items-center gap-y-4 md:gap-x-16">
           {otherServices.map((service, idx) => (
             <div key={idx} className="flex items-center gap-x-4">
               <service.Icon className="size-4 md:size-6 text-yellow-400" />
